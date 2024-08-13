e4e1e2d2f452670c0930e9ba90208c7ed70d2278
Reinvy <reinvy.dev@gmail.com>
2024-08-13 21:48:41 +0700

    setup fillable at models

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'trx_id',
+        'phone_number',
+        'proof',
+        'address',
+        'started_at',
+        'duration',
+        'ended_at',
+        'is_paid',
+        'delivery_method',
+        'total_amount',
+        'product_id',
+        'store_id',
+    ];
+
+    protected $casts = [
+        'started_at' => 'date',
+        'ended_at' => 'date',
+    ];
 }
